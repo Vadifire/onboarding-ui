115746179297dbe2558b24e9fe2138d5ad7ce2de
Cedric Debelle <cedric.debelle@khoros.com>
2019-05-29 15:33:54 -0500

    Add error logging

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,11 @@ const app = express()
 const port = 9000;
 
 app.get('/', (req, res) => {
-	res.sendFile('index.html', { root: 'src/' })
+	res.sendFile('index2.html', { root: 'src/' }, (err) => {
+		if (err) {
+			console.log(err);
+		}
+	})
 });
 
 app.listen(port, () => console.log("NodeJS has been started on port " + port))
