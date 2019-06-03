fbc745a86c370d30dfddbf3d3e959ba7613ee15e
Cedric Debelle <cedric.debelle@khoros.com>
2019-06-03 14:06:58 -0500

    Separate file for JS

---
+++ src/js/index.js
@@ -0,0 +1,79 @@
+
+const OK_RESPONSE_CODE = 200;
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+	var body = document.getElementsByTagName("body")[0];
+
+	var topDiv = document.createElement("div");
+	topDiv.style = "text-align:center;";
+
+	var helloWorldDiv = document.createElement("div");
+	helloWorldDiv.appendChild(document.createTextNode("Hello world"));
+
+	var buttonDiv = document.createElement("div");
+	var button = document.createElement("button");
+	button.setAttribute("onclick", "getHomeTimeline()");
+	button.innerHTML = "Update Home Timeline";
+	buttonDiv.appendChild(button);
+
+	topDiv.appendChild(helloWorldDiv);
+	topDiv.appendChild(buttonDiv);
+
+	var tweetsDiv = document.createElement("div");
+	tweetsDiv.setAttribute("id", "tweets");
+
+	body.appendChild(topDiv);
+	body.appendChild(tweetsDiv);
+	getHomeTimeline();
+}
+
+function getHomeTimeline() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == XMLHttpRequest.DONE) {
+			var tweetsDiv = document.getElementById("tweets");
+			if (this.status == OK_RESPONSE_CODE) {
+				tweetsDiv.innerHTML = "";
+				var responseObj = JSON.parse(this.responseText);
+				for (var i = 0; i < responseObj.length; i++) {
+					var tweetDiv = document.createElement("div");
+					if (i % 2 == 0) {
+						tweetDiv.style = "background-color:#e8f5fd;color:black;padding:20px;";
+					} else {
+						tweetDiv.style = "background-color:#e9e9e9;color:black;padding:20px;";
+					}
+
+					var imageElement = document.createElement("img");
+					imageElement.setAttribute("src", responseObj[i].user.profileImageUrl);
+					tweetDiv.appendChild(imageElement);
+
+					tweetDiv.appendChild(
+						document.createTextNode("[" + new Date(responseObj[i].createdAt).toUTCString() + "] ")
+					);
+
+					var messageSpan = document.createElement("span");
+					var messageElement = document.createElement("a");
+					messageElement.innerHTML = responseObj[i].message;
+					messageElement.setAttribute("href", responseObj[i].url);
+					messageElement.setAttribute("target", "_blank");
+					messageSpan.appendChild(messageElement);
+					tweetDiv.appendChild(messageSpan);
+
+					tweetsDiv.appendChild(tweetDiv);
+				}
+			} else {
+				if (this.response) {
+					console.error("Server responded with error: " + this.response);
+					tweetsDiv.innerHTML = this.response;
+				} else {
+					console.error("Connection to tweeting server failed.");
+					tweetsDiv.innerHTML = "Service is temporarily unavailable.";
+				}
+			}
+		}
+	}
+	xhttp.open("GET", "http://localhost:8080/api/1.0/twitter/timeline");
+	xhttp.send();
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const port = 9000;
 
-app.get('/', (req, res) => {
-	res.sendFile('index.html', { root: 'src/' }, (err) => {
+app.get("/", (req, res) => {
+	res.sendFile("index.html", { root: "src/" }, (err) => {
 		if (err) {
 			console.error(err);
-			res.status(500).send('Internal Server Error');
+			res.status(500).send("Internal Server Error");
+		}
+	})
+});
+
+app.get("/src/js/index.js", (req,res) => {
+	res.sendFile("index.js", { root: "src/js/" }, (err) => {
+		if (err) {
+			console.error(err);
+			res.status(500).send("Internal Server Error");
 		}
 	})
 });
