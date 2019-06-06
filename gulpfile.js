a0d530a9cb959cb8a239c9b4fec48fa76f4349f5
Cedric Debelle <cedric.debelle@khoros.com>
2019-06-06 11:05:42 -0500

    Add Sass/gulp files, update gitignore, fix package.json

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+ 
+sass.compiler = require("node-sass");
+ 
+gulp.task("sass", function () {
+	return gulp.src("./src/css/*.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(gulp.dest("./dist/css"));
+});
+ 
+gulp.task("watch", function () {
+	gulp.watch("./src/css/*.scss", gulp.series("sass"));
+});
