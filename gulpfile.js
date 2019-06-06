c60668bf71f622506b51ea5ffd0ba09b29673a0a
Cedric Debelle <cedric.debelle@khoros.com>
2019-06-06 14:40:47 -0500

    Updated tasks to use recommended API

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,14 @@ var webpackConfig = require("./webpack.config.js");
  
 sass.compiler = require("node-sass");
  
-gulp.task("sass", function () {
+function sass() {
 	return gulp.src("./src/css/*.scss")
 		.pipe(sass().on("error", sass.logError))
 		.pipe(gulp.dest("./dist/css"));
-});
+}
 
-gulp.task("dev", function() {
-	gulp.watch("./src/css/*.scss", gulp.series("sass"));
+function dev() {
+	gulp.watch("./src/css/*.scss", gulp.series(sass));
 
 	// Start a webpack-dev-server
 	new WebpackDevServer(webpack(webpackConfig), webpackConfig.devServer)
@@ -22,9 +22,12 @@ gulp.task("dev", function() {
 					console.log(err);
 				}
 			});
-});
+};
 
-gulp.task("hello", function(done) {
+function hello(done) {
 	console.log("Hello World!");
 	done();
-});
+};
+
+exports.hello = hello;
+exports.dev = dev;
