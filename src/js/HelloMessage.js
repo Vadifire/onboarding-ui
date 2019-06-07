566930dc02151d9009bbf050a8a325e832f73a6e
Cedric Debelle <cedric.debelle@khoros.com>
2019-06-07 14:32:12 -0500

    Use 4 spaces for tabs in HelloMessage.js

--- src/js/HelloMessage.js
+++ src/js/HelloMessage.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 
 class HelloMessage extends React.Component {
-  render() {
-    return (
-      <div>
-        hello react!
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>hello react!</div>
+		);
+	}
 }
 
 export default HelloMessage;
