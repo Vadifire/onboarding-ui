5ce8966b53a7fcf7e082565d06c29ee2bb0e97b0
Cedric Debelle <cedric.debelle@khoros.com>
2019-06-14 18:26:51 -0500

    Move date and default user code to TimelineUI. Use shorthand for copying subset of Tweet data

--- src/js/components/TimelineUI.js
+++ src/js/components/TimelineUI.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import TweetBlock from "./TweetBlock";
 import "../../css/components/TimelineUI.scss";
+const _ = require('lodash');
 
 // Presentational Component for Timeline
 export default class TimelineUI extends React.Component {
@@ -45,24 +46,20 @@ export default class TimelineUI extends React.Component {
 	updateTimeline() {
 		this.props.fetchTimeline().then(tweets => {
 			if (tweets.length > 0) {
-				const trimmedTweets = tweets.map(tweet => { // Only copy information we need
-					return {
-							createdAt: tweet.createdAt,
-							message: tweet.message,
-							url: tweet.url,
-							user: {
-								name: tweet.user.name,
-								profileImageUrl: tweet.user.profileImageUrl,
-								twitterHandle: tweet.user.twitterHandle
-							}
-					};
+				const trimmedTweets = tweets.map(tweet => {
+					const ret = _.pick(tweet, "message", "url", "createdAt", "user");
+					ret.createdAt = new Date(ret.createdAt).toLocaleString("en-us", {month: "short", day: "numeric"});
+					if (!ret.user) {
+						ret.user = {name: "Unknown User"};
+					}
+					return ret;
 				});
 				this.setState({tweets: trimmedTweets, message: null});
 			} else {
 				this.setState({tweets : null, message: "Home timeline is empty."});
 			}
 		}).catch(err => {
-			console.log("Failed to retrieve tweets from home timeline. (" + err+")");
+			console.log("Failed to retrieve tweets from home timeline.");
 			this.setState({tweets: null, message: "Failed to fetch tweets from home timeline. Please try again later."});
 		});
 	}
--- src/js/twitter-api.js
+++ src/js/twitter-api.js
@@ -8,15 +8,6 @@ export function fetchHomeTimeline() {
 	return fetch("http://localhost:8080/api/1.0/twitter/timeline").then(response => { // Attempt to fetch tweets
 		return response.json();
 	}).then(tweets => { // Got JSON
-		return tweets.map(tweet => { // Populate default user to avoid reference error
-			if (!tweet.user) {
-				tweet.user = {
-					name: "Unknown User",
-					displayHandle: "",
-					profileImageUrl: ""
-				}
-			}
-			return tweet;
-		});
+		return tweets;
 	});
 }
