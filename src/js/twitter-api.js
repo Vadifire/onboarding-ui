12d88ce9d133e98bbc0f1e32c889717194c69b59
Cedric Debelle <cedric.debelle@khoros.com>
2019-06-14 17:36:59 -0500

    change error log msg

--- src/js/twitter-api.js
+++ src/js/twitter-api.js
@@ -1,8 +1,9 @@
 /*
  * Fetches tweets from home timeline
  *
- * Returns a promise wrapping tweets
+ * Returns a Promise wrapping tweets or a rejected Promise
  */
+
 export function fetchHomeTimeline() {
 	return fetch("http://localhost:8080/api/1.0/twitter/timeline").then(response => { // Attempt to fetch tweets
 		return response.json();
