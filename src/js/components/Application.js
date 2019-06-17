7be7e81e1359d1864712a9b1dedfcadf936fba29
Cedric Debelle <cedric.debelle@khoros.com>
2019-06-17 10:19:20 -0500

    Shorthand length, don't pass in fetchHomeTimeline

--- src/js/components/Application.js
+++ src/js/components/Application.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import HelloMessage from "./HelloMessage";
 import TimelineUI from "./TimelineUI";
-import {fetchHomeTimeline} from "../twitter-api.js";
 import "../../css/components/Application.scss";
 
 // Contains Title, Hello Message, and Timeline UI
@@ -11,7 +10,7 @@ export default class Application extends React.Component {
 			<div>
 				<div id="title">Lab for Cedric</div>
 				<HelloMessage message="hello react!" />
-				<TimelineUI fetchTimeline={fetchHomeTimeline}/>
+				<TimelineUI />
 			</div>
 		);
 	}
