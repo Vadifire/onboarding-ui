fcf0011273e691a2f5516b19b399bc92d844e8e3
Cedric Debelle <cedric.debelle@khoros.com>
2019-06-19 09:23:45 -0500

    Remove unecessary named imp

--- src/main/js/components/Application.js
+++ src/main/js/components/Application.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import HelloMessage from "./HelloMessage";
 import TimelineUI from "./TimelineUI";
-import css from "../../css/components/Application.scss";
+import "../../css/components/Application.scss";
 
 export default class Application extends React.Component {
 	render() {
