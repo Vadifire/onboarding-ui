04e68ed9b00846c8388914293bdca535c08b9d40
Cedric Debelle <cedric.debelle@khoros.com>
2019-06-19 16:53:38 -0500

    rename

--- src/test/js/twitter-api.test.js
+++ src/test/js/twitter-api.test.js
@@ -2,34 +2,34 @@ import * as Api from "../../main/js/twitter-api";
 
 describe("twitter-api", () => {
 
-	let someNum, mockXHR;
+	let mockedRequest;
 
 	beforeAll(() => {
-		mockXHR = {
+		mockedRequest = {
 			open: jest.fn(),
 			send: jest.fn(),
 			readyState: XMLHttpRequest.DONE,
 			DONE: XMLHttpRequest.DONE
 		};
-		window.XMLHttpRequest = jest.fn(() => mockXHR);
+		window.XMLHttpRequest = jest.fn(() => mockedRequest);
 	});
 
 	afterEach(() => { // Ensure XHR retrieves from correct endpoint
-		expect(mockXHR.open).toHaveBeenCalledTimes(1);
-		expect(mockXHR.open).toHaveBeenCalledWith("GET", Api.homeTimelineEndpoint);
-		mockXHR.open.mockClear();
-		expect(mockXHR.send).toHaveBeenCalledTimes(1);
-		mockXHR.send.mockClear();
+		expect(mockedRequest.open).toHaveBeenCalledTimes(1);
+		expect(mockedRequest.open).toHaveBeenCalledWith("GET", Api.homeTimelineEndpoint);
+		mockedRequest.open.mockClear();
+		expect(mockedRequest.send).toHaveBeenCalledTimes(1);
+		mockedRequest.send.mockClear();
 	});
 
 	test("should attempt to fetch tweets and on reject execute callback with null", done => {
 		
-		mockXHR.responseText = "Invalid JSON"; // Return invalid JSON
+		mockedRequest.responseText = "Invalid JSON"; // Return invalid JSON
 		Api.fetchHomeTimeline(response => {
 			expect(response).toBeNull();
 			done();
 		});
-		mockXHR.onreadystatechange();
+		mockedRequest.onreadystatechange();
 	});
 
 	test("should fetch from Api, parse tweets from JSON, and execute callback with tweets", done => {
@@ -37,12 +37,12 @@ describe("twitter-api", () => {
 			message: "some message"
 		}];
 
-		mockXHR.responseText = JSON.stringify(tweets); // Set response to valid tweets
+		mockedRequest.responseText = JSON.stringify(tweets); // Set response to valid tweets
 		Api.fetchHomeTimeline(response => {
 			expect(response).toEqual(tweets);
 			done();
 		});
-		mockXHR.onreadystatechange();
+		mockedRequest.onreadystatechange();
 	});
 
 });
